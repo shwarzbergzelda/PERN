2442dd63ea5b33357c0ddad57b0836418de79c6f
zelda <shwarzbergzelda@gmail.com>
2026-05-03 11:22:49 -0400

    implement react and middleware

---
+++ pern-cc-starter/04_reactjs/src/App.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import Car from "./components/Car";
+
+const App = () => {
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/v1/cars")
+      .then((res) => res.json())
+      .then((data) => setCars(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome to the Car Store</h1>
+      <ul>
+        {cars.map((car) => (
+          <Car key={car.id} {...car} />
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
---
+++ pern-cc-starter/04_reactjs/src/components/Car.jsx
@@ -0,0 +1,12 @@
+const Car = ({make, model, year, price}) => {
+  return (
+    <li>
+      <p>Make: {make}</p>
+      <p>Model: {model}</p>
+      <p>Year: {year}</p>
+      <p>Price: {price}</p>
+    </li>
+  )
+}
+
+export default Car
